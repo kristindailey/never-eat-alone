04e9d470c4742c2d9c607001c7c3a739572edd4f
Kristin Dailey <kristin.dailey02@gmail.com>
2025-02-17 05:11:32 -0800

    Added add meetup page

--- src/components/MeetupListings.jsx
+++ src/components/MeetupListings.jsx
@@ -1,47 +1,23 @@
-// import React, { useState, useEffect } from "react";
-// import MeetupListing from "./MeetupListing";
+import React, { useState, useEffect } from "react";
+import MeetupListing from "./MeetupListing";
 // import { meetupServices } from "../services/meetupServices";
 
-// const MeetupListings = () => {
-//   const [meetups, setMeetups] = useState([]);
-//   const [loading, setLoading] = useState(true);
+const MeetupListings = () => {
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          Browse Meetups
+        </h2>
 
-//   useEffect(() => {
-//     const fetchMeetups = async () => {
-//       try {
-//         const response = await meetupServices.getAllMeetups();
-//         const meetupArray = Object.values(response.documents);
-//         const recentMeetups = meetupArray.slice(0, 3);
-//         setMeetups(recentMeetups);
-//       } catch (error) {
-//         console.error("Error fetching meetups:", error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {meetups.map((meetup) => (
+              <MeetupListing key={meetup.id} meetup={meetup} />
+            ))}
+        </div>
+      </div>
+    </section>
+  )
+};
 
-//     fetchMeetups();
-//   }, []);
-
-//   if (loading) {
-//     return <div>Loading...</div>
-//   }
-
-//   return (
-//     <section className="bg-blue-50 px-4 py-10">
-//       <div className="container-xl lg:container m-auto">
-//         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-//           Browse Meetups
-//         </h2>
-
-//         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-//             {meetups.map((meetup) => (
-//               <MeetupListing key={meetup.id} meetup={meetup} />
-//             ))}
-//         </div>
-//       </div>
-//     </section>
-//   )
-// };
-
-// export default MeetupListings;
+export default MeetupListings;
--- src/pages/AddMeetupPage.jsx
+++ src/pages/AddMeetupPage.jsx
@@ -1,8 +1,87 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddMeetupPage = () => {
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("Full-Time");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [salary, setSalary] = useState("Under $50K");
+  const [companyName, setCompanyName] = useState("");
+  const [companyDescription, setCompanyDescription] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+  const [contactPhone, setContactPhone] = useState("");
+
+  const navigate = useNavigate();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const newMeetup = {
+        title, 
+        type,
+        location,
+        description,
+        salary,
+        company: {
+            name: companyName,
+            description: companyDescription,
+            contactEmail, 
+            contactPhone,
+        },
+    };
+
+    addJobSubmit(newJob);
+
+    toast.success("Job added successfully.");
+
+    return navigate("/jobs");
+};
+
   return (
-    <div>AddMeetupPage</div>
+    <section className="bg-indigo-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <form onSubmit={submitForm}>
+            <h2 className="text-3xl text-center font-semibold mb-6">Add Meetup</h2>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">Meetup Listing Name</label>
+              <input type="text" id="title" name="title" className="border rounded w-full py-2 px-3 mb-2" placeholder="e.g., Banki Over Lunch" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="description" className="block text-gray-700 font-bold mb-2">Description</label>
+              <textarea id="description" name="description" className="border rounded w-full py-2 px-3" rows="4" placeholder="List your intention (e.g., get to know each other, study, interview prep, etc.) and anything you might want to discuss over lunch." value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+            </div>
+
+            <div className='mb-4'>
+              <label className='block text-gray-700 font-bold mb-2'>
+                Meeting Date
+              </label>
+              <input type='text' id='location' name='location' className='border rounded w-full py-2 px-3 mb-2' placeholder='e.g., March 31, 2025' required value={location} onChange={(e) => setLocation(e.target.value)}/>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="company" className="block text-gray-700 font-bold mb-2">Meeting Time</label>
+              <input type="text" id="company" name="company" className="border rounded w-full py-2 px-3" placeholder="e.g., 12:00 PM" value={companyName} onChange={(e) => setCompanyName(e.target.value)}/>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="contact_email" className="block text-gray-700 font-bold mb-2">Your Time Zone</label>
+              <input type="email" id="contact_email" name="contact_email" className="border rounded w-full py-2 px-3" placeholder="e.g., PST" required value={contactEmail} onChange={(e) => setContactEmail(e.target.value)}/>
+            </div>
+
+            <div>
+              <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline" type="submit">
+                Add Meetup
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 
